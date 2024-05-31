8f5d4316ec1489a8725679774eaf0f2a6faef14c
Tito Carlos <titocarlos080@gmail.com>
2024-05-31 15:36:03 -0400

    arreglos detallados

--- target/classes/bo/com/titodev/Models/script.sql
+++ target/classes/bo/com/titodev/Models/script.sql
@@ -274,3 +274,17 @@ INSERT INTO notas (estudiante_id, docente_id, grupo_sigla, nota_final) VALUES
 (4100.90, '2023-04-05', 'Mantenimiento del edificio', 1)
  
 
+
+
+
+
+
+// eliminacion de tablas
+DO $$ 
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
